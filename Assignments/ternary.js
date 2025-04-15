c34bc7d75fdbf3b01283c04d6c7f543134bca8c7
ibbo2003 <ibrahimhakim.ih@gmail.com>
2025-04-15 19:30:14 +0530

    Ternary Assignment

---
+++ Assignments/ternary.js
@@ -0,0 +1,4 @@
+let number = [5, -2, 0, 8, -7];
+let result = number.map((n) => {
+    console.log(n == 0 ? 'zero and even': n>0 && n%2==0? 'positive and even':n>0 ?'positive and odd':n<0 && n%2 == 0 ? 'negative and even':'negative and odd')
+})
